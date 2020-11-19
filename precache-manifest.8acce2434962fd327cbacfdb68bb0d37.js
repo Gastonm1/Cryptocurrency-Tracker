454d1dc1d26d3d0511f826b94b25e58177b48dac
Gastonm1 <gaston.marques@gmail.com>
2020-11-18 22:25:51 -0600

    Updates

---
+++ precache-manifest.8acce2434962fd327cbacfdb68bb0d37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651f1f473e99c91a4af3400c4962d2ef",
+    "url": "/Cryptocurrency-Tracker/index.html"
+  },
+  {
+    "revision": "0f2a966563f406c4127c",
+    "url": "/Cryptocurrency-Tracker/static/css/main.3728b7b4.chunk.css"
+  },
+  {
+    "revision": "e2e9a31129ac98e6f884",
+    "url": "/Cryptocurrency-Tracker/static/js/2.a3416827.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cryptocurrency-Tracker/static/js/2.a3416827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2a966563f406c4127c",
+    "url": "/Cryptocurrency-Tracker/static/js/main.6b313d97.chunk.js"
+  },
+  {
+    "revision": "2a553e492ff1999a3430",
+    "url": "/Cryptocurrency-Tracker/static/js/runtime-main.2f90922e.js"
+  }
+]);
